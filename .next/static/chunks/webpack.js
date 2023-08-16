77110a447bc22a726ea6d3aecf690ac885d398aa
CodeBreaker <32624090+Xiao009@users.noreply.github.com>
2023-08-16 10:54:20 -0400

    update config file again

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "0e5ee28b3445ddfa"; }
+/******/ 		__webpack_require__.h = function() { return "4db8ba5a643be5a1"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
