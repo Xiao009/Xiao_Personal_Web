472324163457c9fb9944ede608a641c51f9bd8af
CodeBreaker <32624090+Xiao009@users.noreply.github.com>
2023-08-16 10:45:59 -0400

    chnage
    Move config file position

--- pages/next.config.tsx
+++ pages/next.config.tsx
@@ -1,11 +1,11 @@
 module.exports = {
-    webpack: function (config:any) {
-      config.module.rules.push({
-        test: /\.md$/,
-        use: 'raw-loader',
-      })
-      return config
-    },
-  }
+  webpack: function (config: any) {
+    config.module.rules.push({
+      test: /\.md$/,
+      use: "raw-loader",
+    });
+    return config;
+  },
+};
 
-  export {};
+export {};
