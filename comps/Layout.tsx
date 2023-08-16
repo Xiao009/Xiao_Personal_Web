2ba2ab8ea41e4aaebe139301fc9d501210f9fb5b
CodeBreaker <32624090+Xiao009@users.noreply.github.com>
2023-08-16 10:38:12 -0400

    Update small
    delect the testcase

--- comps/Layout.tsx
+++ comps/Layout.tsx
@@ -1,25 +1,23 @@
-import Navbar from './Navbar'
-import Footer from './Footer'
-import { createTheme, ThemeProvider } from '@mui/material/styles';
-import CssBaseline from '@mui/material/CssBaseline';
-import Container from '@mui/material/Container';
+import Navbar from "./Navbar";
+import Footer from "./Footer";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+import CssBaseline from "@mui/material/CssBaseline";
+import Container from "@mui/material/Container";
 
- 
 //加栏目content在里面
 const sections = [
-  { title: 'HOME', url: '/' },
-  { title: 'about', url: '/about' },
-  { title: 'testcase', url: '/testcase' },
-  { title: 'data grid', url: '/datagrid' },
-  { title: 'example1', url: '/#' },
+  { title: "HOME", url: "/" },
+  { title: "about", url: "/about" },
+  { title: "testcase", url: "/" },
+  { title: "data grid", url: "/datagrid" },
+  { title: "example1", url: "/#" },
 ];
 
 const defaultTheme = createTheme();
 
 interface LayoutProps {
-    children: React.ReactNode;
-  }
-
+  children: React.ReactNode;
+}
 
 const Layout: React.FC<LayoutProps> = ({ children }) => {
   return (
