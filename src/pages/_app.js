863501890f06f64183e4dbda08548eedfd9d86a8
CodeBreaker <32624090+Xiao009@users.noreply.github.com>
2023-08-16 23:23:44 -0400

    update

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,10 +1,9 @@
-import '@/styles/globals.css' // this is for global change for look
-import Layout from '../../comps/layout' 
+import "../styles/globals.css"; // this is for global change for look
+import Layout from "../../comps/layout";
 export default function App({ Component, pageProps }) {
   return (
     <Layout>
       <Component {...pageProps} />
     </Layout>
-  )
+  );
 }
- 
